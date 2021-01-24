81a52d718278ac2136302caf8fcb4a4fbbd0d9c5
simone celia <simone.celia@thinkopen.it>
2021-01-24 21:57:55 +0100

    header

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="">
-	<meta name="author" content="">
+	<meta name="description" content="comarea site">
+	<meta name="author" content="simone celia">
 	<title>COMAREA Cuina L’Hocal</title>
 	<!-- Bootstrap core CSS -->
 	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
