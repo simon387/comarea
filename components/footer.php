2512561725124f9eedc12e1fa32143b9ceb814b6
simone <simon387@hotmail.it>
2021-11-18 19:26:10 +0100

    1.2.1

--- components/footer.php
+++ components/footer.php
@@ -5,7 +5,7 @@ fclose($f);
 ?>
 <footer class="footer text-faded text-center py-5">
 	<div class="container">
-		<p class="m-0 small">Copyright &copy; <a href="www.simonecelia.it">www.simonecelia.it</a> 2021. <?php echo $version ?></p>
+		<p class="m-0 small">Copyright &copy; 2021 <a href="www.simonecelia.it">www.simonecelia.it</a>. <?php echo $version ?></p>
 	</div>
 </footer>
 <script src="vendor/jquery/jquery.min.js"></script>
