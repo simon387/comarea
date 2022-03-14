5acf538cd052e5818cdb86850a41fca60cd9873d
simone <simone.celia@csi.it>
2022-03-14 09:58:00 +0100

    job4tati

---
+++ products_tati.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="comarca site">
+	<meta name="author" content="simone celia">
+	<title>Grenyaobrador.es</title>
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="css/business-casual.min.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet">
+	<link rel="shortcut icon" href="img/ico.png"/>
+</head>
+<body>
+<h1 class="site-heading text-center text-white d-none d-lg-block">
+	<span class="site-heading-upper text-primary mb-3">Elaboracion y venta de productos de panaderia y pasteleria</span>
+	<span class="site-heading-lower">Grenyaobrador.es</span>
+</h1>
+
+<style>
+	.gordo {
+		font-size: 1.5rem;
+	}
+</style>
+
+<section class="page-section">
+	<div class="container">
+		<div class="product-item">
+			<div class="product-item-title d-flex">
+				<div class="bg-faded p-5 d-flex ml-auto rounded">
+					<h2 class="section-heading mb-0">
+						<span class="section-heading-upper">TRACTAMENTS DELICIOSOS, BON MENJAR</span>
+						<span class="section-heading-lower">Pa Alemany</span>
+					</h2>
+				</div>
+			</div>
+			<img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="1.jpg" alt="productos" height="1400px">
+			<div class="product-item-description d-flex mr-auto">
+				<div class="bg-faded p-5 rounded">
+					<p class="mb-0 gordo">Farina de blat, llavors (avena,sesam,lli,pipes,rosella), massa mare, aigua,malta,sal y llevat</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="page-section">
+	<div class="container">
+		<div class="product-item">
+			<div class="product-item-title d-flex">
+				<div class="bg-faded p-5 d-flex mr-auto rounded">
+					<h2 class="section-heading mb-0">
+						<span class="section-heading-upper">TRACTAMENTS DELICIOSOS, BON MENJAR</span>
+						<span class="section-heading-lower">Pa de cereals</span>
+					</h2>
+				</div>
+			</div>
+			<img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="2.jpg" alt="productos" height="1400px">
+			<div class="product-item-description d-flex ml-auto">
+				<div class="bg-faded p-5 rounded">
+					<p class="mb-0 gordo">Farina de blat,llavors(avena,sesam,lli,pipes, rosella), massa mare,aigua, sal,llevat
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="page-section">
+	<div class="container">
+		<div class="product-item">
+			<div class="product-item-title d-flex">
+				<div class="bg-faded p-5 d-flex ml-auto rounded">
+					<h2 class="section-heading mb-0">
+						<span class="section-heading-upper">From Around the World</span>
+						<span class="section-heading-lower">Pa Gallec</span>
+					</h2>
+				</div>
+			</div>
+			<img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="3.jpg" alt="productos" height="1400px">
+			<div class="product-item-description d-flex mr-auto">
+				<div class="bg-faded p-5 rounded">
+					<p class="mb-0 gordo">Farina de blat, aigua, llevat, sal, massa mare</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="page-section">
+	<div class="container">
+		<div class="product-item">
+			<div class="product-item-title d-flex">
+				<div class="bg-faded p-5 d-flex ml-auto rounded">
+					<h2 class="section-heading mb-0">
+						<span class="section-heading-upper">TRACTAMENTS DELICIOSOS, BON MENJAR</span>
+						<span class="section-heading-lower">Pa d'espelta integral</span>
+					</h2>
+				</div>
+			</div>
+			<img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="4.jpg" alt="productos" height="1400px">
+			<div class="product-item-description d-flex mr-auto">
+				<div class="bg-faded p-5 rounded">
+					<p class="mb-0 gordo">Farina d'espelta integral, aigua, preferment poolish, sal</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="page-section">
+	<div class="container">
+		<div class="product-item">
+			<div class="product-item-title d-flex">
+				<div class="bg-faded p-5 d-flex mr-auto rounded">
+					<h2 class="section-heading mb-0">
+						<span class="section-heading-upper">TRACTAMENTS DELICIOSOS, BON MENJAR</span>
+						<span class="section-heading-lower">Pa de coca</span>
+					</h2>
+				</div>
+			</div>
+			<img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="5.jpg" alt="productos" height="1400px">
+			<div class="product-item-description d-flex ml-auto">
+				<div class="bg-faded p-5 rounded">
+					<p class="mb-0 gordo">Farina de blat, aigua, massa mare, llevat, sal
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="page-section">
+	<div class="container">
+		<div class="product-item">
+			<div class="product-item-title d-flex">
+				<div class="bg-faded p-5 d-flex ml-auto rounded">
+					<h2 class="section-heading mb-0">
+						<span class="section-heading-upper">From Around the World</span>
+						<span class="section-heading-lower">Pa Baviera</span>
+					</h2>
+				</div>
+			</div>
+			<img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="6.jpg" alt="productos" height="1400px">
+			<div class="product-item-description d-flex mr-auto">
+				<div class="bg-faded p-5 rounded">
+					<p class="mb-0 gordo">Farina d'espelta integral, aigua, farina de blat, massa mare, farina de segol, llevat</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="page-section">
+	<div class="container">
+		<div class="product-item">
+			<div class="product-item-title d-flex">
+				<div class="bg-faded p-5 d-flex ml-auto rounded">
+					<h2 class="section-heading mb-0">
+						<span class="section-heading-upper">TRACTAMENTS DELICIOSOS, BON MENJAR</span>
+						<span class="section-heading-lower">Pa de pages</span>
+					</h2>
+				</div>
+			</div>
+			<img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="7.jpg" alt="productos" height="1400px">
+			<div class="product-item-description d-flex mr-auto">
+				<div class="bg-faded p-5 rounded">
+					<p class="mb-0 gordo">Farina de blat, aigua, sal, llevat</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<footer class="footer text-faded text-center py-5">
+	<div class="container">
+		<p class="m-0 small">Plaça francesc Macia i Lluisa, 2. L'Hospitalet de llobregat</p>
+		<p class="m-0 small">Grenyaobrador@gmail.com</p>
+	</div>
+</footer>
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
